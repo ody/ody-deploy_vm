132e31d55e666977097d1879cdfaef46ff91943a
Cody Herriges <193064+ody@users.noreply.github.com>
2021-05-13 21:18:21 +0000

    Ensuring Windows deployment is possible

--- lib/puppet/functions/deploy_vm/tempfile.rb
+++ lib/puppet/functions/deploy_vm/tempfile.rb
@@ -1,10 +1,10 @@
-require 'tempfile'
+require 'tmpdir'
 Puppet::Functions.create_function(:'deploy_vm::tempfile') do
   dispatch :tempfile do
     param 'String', :name
     param 'String', :path
   end
   def tempfile(name, path)
-    return Tempfile.new(name, path).path
+    return Dir::Tmpname.create(name, path) { || }
   end
 end
--- plans/init.pp
+++ plans/init.pp
@@ -1,26 +1,70 @@
+# @summary
+#   A Bolt Plan that facilitates the creation of a virtual machine on a
+#   specific selection of cloud providers by orchestrating the execution of
+#   Terraform stateless by throwing away the working directoy post deploy.
+#   Intended use case is as a simple VM provisioning solution within a more
+#   thorough workflow which will track resources in an auxillary way.
+#
+# @see https://github.com/ody/ody-deploy_vm
+#
+# @example Greate a new virtual machine within a GCP project
+#   puppet plan run deploy_vm targets=pe-primary.example.com ssh_user=provisioner network=default provider=gcp vm_name=example
+#
+# @param vm_image
+#   Name of the image that the virtual machine will be cloned from
+#
+# @param vm_name
+#   A predictable base name prepended to the virtual machine in addition to the
+#   generated random identifier
+#
+# @param network
+#   Network that that virtual machine will be attached to
+#
+# @param cloud_region
+#   Cloud region when deploying to any public cloud provider
+#
+# @param project
+#   An authenticated project where virutal machine will be provisioned when
+#   deploying to Google Cloud Platform
+#
+# @param user
+#   Remote user for validating newly provisioned host accepts connections,
+#   enabling the next step of any process to proceed
+#
+# @param ssh_key_file
+#   Path to an SSH private key which will be used when validating a virtual
+#   machine running linux is ready to accept connections
+#
+# @param provider
+#   Which cloud provider to provision a new virtual machine to, Google Cloud
+#   Platform, VMWare vCenter, etc, etc.
+#
 plan deploy_vm(
-  TargetSpec            $targets,
-  Optional[String[1]]   $ssh_key_file = undef,
-  Optional[String[1]]   $vm_image     = undef,
-  Optional[String[1]]   $vm_name      = undef,
-  Optional[String[1]]   $network      = undef,
-  String[1]             $cloud_region = 'us-west1',
-  String[1]             $project,
-  String[1]             $ssh_user,
-  Enum['gcp', 'vmware'] $provider
+  TargetSpec               $targets,
+  Optional[String[1]]      $ssh_key_file = undef,
+  Optional[String[1]]      $winrm_pw     = undef,
+  Optional[String[1]]      $vm_image     = undef,
+  Optional[String[1]]      $vm_name      = undef,
+  Optional[String[1]]      $network      = undef,
+  String[1]                $cloud_region = 'us-west1',
+  String[1]                $project,
+  String[1]                $remote_user,
+  Enum['gcp', 'vmware']    $provider,
+  Enum['linux', 'windows'] $os_type
 ) {
 
   # Dynamic generation of tfvars file from a Puppet EPP template and store it in
   # variable that can be used later
   $tfvars = $provider ? {
-    'gcp'     => epp('deploy_vm/gcp.tfvars.epp', {
+    'gcp'     => epp("deploy_vm/${os_type}/gcp.tfvars.epp", {
                    'project'       => $project,
-                   'ssh_user'      => $ssh_user,
+                   'user'          => $remote_user,
                    'ssh_key_file'  => $ssh_key_file,
+                   'winrm_pw'      => $winrm_pw,
                    'cloud_region'  => $cloud_region,
                    'vm_image'      => $vm_image,
                    'vm_name'       => $vm_name,
-                   'network'       => $network,
+                   'network'       => $network
                  }),
     'vmware'  => undef,
     default   => undef
@@ -36,7 +80,7 @@ plan deploy_vm(
 
   # Store contents of Terraform manifest in a variable to keep consistent with
   # tfvars file generation
-  $tffile = epp("deploy_vm/${provider}.tf.epp")
+  $tffile = epp("deploy_vm/${os_type}/${provider}.tf.epp")
 
   # Simple function that generates a unique path name at /tmp that can be
   # created on a target to stage Terraform content within
